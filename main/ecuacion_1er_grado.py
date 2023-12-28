82da9b7c1f1e1d08e8e7e5148b2487c071ab7a58
EzeGamer135 <73393487+EzeGamer135@users.noreply.github.com>
2023-12-28 15:09:40 -0300

    Add files via upload

---
+++ main/ecuacion_1er_grado.py
@@ -0,0 +1,45 @@
+# función
+
+def ecu(a, b, c, x):  # 'x' es el número a la derecha de la igualdad
+    if a == 0:  # 'a' no existe
+
+        if c == 0:  # 'c' no existe
+            return x / b
+        else:  # 'c' existe
+            if c < 0:  # 'c' es menor que 0
+                return (x + c) / b
+            else:  # 'c' es mayor que 0
+                return (x - c) / b
+    else:
+        if a < 0:  # 'a' es menor que 0
+            if c == 0:  # 'c' no existe
+
+                return (x + a) / b
+            elif c < 0:  # 'c' es menor que 0
+                return (x + a + c) / b
+            else:  # 'c' es mayor que 0
+                return (x + a - c) / b
+
+        else:  # 'a' es mayor que 0
+            if c == 0:  # 'c' no existe
+                return (x - a) / b
+
+            elif c < 0:  # 'c' es menor que 0
+                return (x - a + c) / b
+            else:  # 'c' es mayor que 0
+                return (x - a - c) / b
+
+
+# entradas
+
+entradas = input("Valores establecidos (a, b(x), c, x): ")
+entradas0 = entradas.split(',')
+if len(entradas0) == 4:
+    a0, b0, c0, x0 = entradas0[:4]
+    a0 = int(a0.strip())
+    b0 = int(b0.strip())
+    c0 = int(c0.strip())
+    x0 = int(x0.strip())
+    print("b = " + str(float(ecu(a0, b0, c0, x0))))
+else:
+    print("Debe ingresar 4 valores.")
---
+++ main/interes_compuesto.py
@@ -0,0 +1,19 @@
+# función
+
+
+def monto(vp, ip, n):
+    return (((1 + (ip / 100)) ** (12 * n)) - 1) * vp
+
+
+# entradas
+
+entradas = input("Valores establecidos (Valor presente, Tasa de interés (%), Períodos): ")
+entradas0 = entradas.split(',')
+if len(entradas0) == 3:
+    vp0, ip0, n0 = entradas0[:3]
+    vp0 = float(vp0.strip())
+    ip0 = float(ip0.strip())
+    n0 = float(n0.strip())
+    print("El interés ganado asciende a $" + str(round(monto(vp0, ip0, n0), 2)))
+else:
+    print("Debe ingresar 3 valores.")
