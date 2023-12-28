99d2f14d9f7f19f9f782d0c8bdeddbf6ecef8e38
EzeGamer135 <73393487+EzeGamer135@users.noreply.github.com>
2023-12-28 15:23:57 -0300

    Update ley_de_coseno.py

--- main/ley_de_coseno.py
+++ main/ley_de_coseno.py
@@ -7,7 +7,7 @@ def coseno(b, c, A):
     return sqrt((b ** 2 + c ** 2) - (2 * b * c * cos(A)))
 # entradas
 
-inputs = input("Valores establecidos (lado b, lado c, ángulo A): ")
+inputs = input("(lado b, lado c, ángulo A): ")
 inputs_sep = inputs.split(',')
 if len(inputs_sep) == 3:
     b0, c0, A0 = inputs_sep[:3]
